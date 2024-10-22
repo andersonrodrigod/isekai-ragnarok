1991d793fe5f7c07d36b795ad7de82db8a53182c
ANDERSON RODRIGO <99427400+andersonrodrigod@users.noreply.github.com>
2024-10-21 21:35:09 -0300

    att

--- pesquisar_palavra.py
+++ pesquisar_palavra.py
@@ -41,7 +41,7 @@ def pesquisar_palavra_na_pasta(pasta, palavra):
 pasta_arquivos = './capitulos_en'
 
 # Palavra a ser pesquisada
-palavra_procurada = 'novice'
+palavra_procurada = 'field'
 
 # Caminho da pasta de saída
 pasta_pesquisa = 'pesquisar'
